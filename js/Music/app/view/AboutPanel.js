788eb7e958e804544a8b3657e0599290f212c334
Dave Ackerman <dmackerman@gmail.com>
2012-01-31 15:04:52 -0500

    minor css

--- js/Music/app/view/AboutPanel.js
+++ js/Music/app/view/AboutPanel.js
@@ -14,12 +14,26 @@ Ext.define('Music.view.AboutPanel', {
         centered : true,
         hideOnMaskTap: true,
         floating: true,
-        width: 700,
-        height: 700,
+        width: 600,
+        height: 500,
         items: [{
             html: '<h1>About Discover Music</h1>'
         },{
-            html: 'Discover Music is built using Sencha Touch 2, an HTML5 mobile framework developed by Sencha, Inc.'
+            html: '<p>Discover Music is built using Sencha Touch 2, an HTML5 mobile framework developed by Sencha, Inc.</p>' +
+                  '<p>Discover Music was built by Modus Create, a hybrid application development company based out of Washington, DC.</p>' +
+                  '<p>All content and audio clips are courtesy of NPR, and are accessed using their free and open public API’s.</p>'
+        },{
+            xtype: 'button',
+            text: 'Donate to NPR',
+            ui: 'action'
+        },{
+            xtype: 'button',
+            text: 'Sencha Touch Website',
+            ui: 'confirm'
+        },{
+            xtype: 'button',
+            text: 'Modus Create Website',
+            ui: 'normal'
         }]      
     },
 
