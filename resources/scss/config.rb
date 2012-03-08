b3f81106b7fccb5b33fec6acefa2d368c2679836
Dave Ackerman <dmackerman@gmail.com>
2012-03-07 22:17:20 -0500

    new config.rb with correct paths for sass

--- resources/scss/config.rb
+++ resources/scss/config.rb
@@ -2,7 +2,7 @@
 dir = File.dirname(__FILE__)
 
 # Load the sencha-touch framework automatically.
-load File.join(dir, '..', '..', '..', 'sencha-touch-2.0', 'resources', 'themes')
+load File.join(dir, '..', '..', 'sdk', 'resources', 'themes')
 
 # Compass configurations
 sass_path    = dir
