26d233ee6e8d325eaea21a560d8711777ab1473a
Jay Garcia <jgarcia@tdg-i.com>
2012-03-16 15:39:58 -0400

    Station finder stuff

--- app/view/StationDetail.js
+++ app/view/StationDetail.js
@@ -1,10 +1,22 @@
 Ext.define('Music.view.StationDetail', {
     extend : 'Ext.Container',
+    xtype : 'stationdetail',
     config : {
-        data : { name : 'No station selected!' },
-        tpl : [
-            '<div>',
-                '{name}',
+        scrollable : true,
+        tpl        : [
+            '<div style="padding: 10px;">',
+                '<h1 style="font-weight: bold;">{name}</h1>',
+                '<div>{tagLine}</div>',
+                '<div>Frequency: {band} {frequency}</div>',
+                '<div>Location: {marketCity} {state}</div>',
+                '<tpl for="urls">',
+                    '<div>',
+                        '{type}',
+                    '</div>',
+                '</tpl>',
+                '<tpl if=" ! values.urls">',
+                    '<div>No online streams available</div>',
+                '</tpl>',
             '</div>'
         ]
     }
--- nprStationFinder.php
+++ nprStationFinder.php
@@ -2,25 +2,38 @@
 
 $lat = $_REQUEST['lat'];
 $lon = $_REQUEST['lon'];
-$url = "http://api.npr.org/stations.php?apiKey=MDA4ODE2OTE5MDEzMjYwODI4NDdiOGU5Yw001";
+$zip = $_REQUEST['zip'];
 
+$url = "http://api.npr.org/stations.php?apiKey=MDA4ODE2OTE5MDEzMjYwODI4NDdiOGU5Yw001";
+$ok = false;
 
 if ($lat && $lon) {
-        $url .= "&lon=$lon&lat=$lat";
-        // fetch XML
-        $c = curl_init();
-        curl_setopt_array($c, array(
-                CURLOPT_URL => $url,
-                CURLOPT_HEADER => false,
-                CURLOPT_TIMEOUT => 10,
-                CURLOPT_RETURNTRANSFER => true
-        ));
-        $r = curl_exec($c);
-        curl_close($c);
+    $url .= "&lon=$lon&lat=$lat";
+    $ok = true;
 }
+
+if ($zip) {
+    $url .= "&zip=$zip";
+    $ok = true;
+}
+
+if ($ok) {
+	echo $url;
+    // fetch XML
+    $c = curl_init();
+    curl_setopt_array($c, array(
+        CURLOPT_URL => $url,
+        CURLOPT_HEADER => false,
+        CURLOPT_TIMEOUT => 10,
+        CURLOPT_RETURNTRANSFER => true
+    ));
+    $r = curl_exec($c);
+    curl_close($c);
+}
+
 if ($r) {
-    echo json_encode(new SimpleXMLElement($r));
+    echo $r;
 }
 else {
-    echo "WTF";
+    echo "{}";
 }
