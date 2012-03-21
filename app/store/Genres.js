440c9762a5532468ec921d5a78b83bd584c73232
Jay Garcia <jgarcia@tdg-i.com>
2012-03-21 10:39:39 -0400

    Merge branch 'master' of github.com:ModusCreateOrg/NPRMusic into dev
    Conflicts:
    	app/view/Article.js
    
    Staging the NPR station feed

--- app/store/Genres.js
+++ app/store/Genres.js
@@ -6,19 +6,57 @@
  * The Genres store
  */
 Ext.define('Music.store.Genres', {
-    extend: 'Ext.data.Store',
+    extend : 'Ext.data.Store',
 
-    config		: {
-		model		: 'Music.model.Genre',
-		
-		proxy		: {
-			type	: 'ajax',
-			url		: 'serverside/genres.json',
-			reader	: {
-				type			: 'json',
-				rootProperty	: 'data'
-			}
-		}
-	}
+    config : {
+        model : 'Music.model.Genre',
+
+        proxy : {
+            type : 'memory',
+            data : [
+                {
+                    "id"   : 10001,
+                    "key"  : "rockPopFolk",
+                    "name" : "Rock / Pop / Folk"
+                },
+                {
+                    "id"   : 10003,
+                    "key"  : "classical",
+                    "name" : "Classical"
+                },
+                {
+                    "id"   : 10002,
+                    "key"  : "jazzBlues",
+                    "name" : "Jazz & Blues"
+                },
+                {
+                    "id"   : 10004,
+                    "key"  : "world",
+                    "name" : "World"
+                },
+                {
+                    "id"   : 10005,
+                    "key"  : "hipHopRB",
+                    "name" : "Hip-Hop / R&B"
+                },
+                {
+                    "id"   : 135408474,
+                    "key"  : "electronicDance",
+                    "name" : "Electronic"
+                },
+                {
+                    "id"   : 92792712,
+                    "key"  : "country",
+                    "name" : "Country"
+                },
+                {
+                    "id"   : 139996449,
+                    "key"  : "latinAlternative",
+                    "name" : "Latin"
+                }
+            ]
+
+        }
+    }
 
 });
