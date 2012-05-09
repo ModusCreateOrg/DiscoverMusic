1f4c25b2302879039e451562add75c9580206d20
Jay Garcia <jgarcia@tdg-i.com>
2012-05-09 12:09:58 -0400

    Ensured the search store uses discovermusic.moduscreate.com

--- app/store/Search.js
+++ app/store/Search.js
@@ -5,34 +5,7 @@
  *
  * Description: Search page store
  */
-//
-//Ext.define('Music.store.Search', {
-//    extend   : 'Ext.data.Store',
-//    alias    : 'store.search',
-//    config   : {
-//        model : 'Music.model.Article',
-//        proxy : {
-//            type        : 'jsonp',
-//            url         : 'http://api.npr.org/query',
-//            callbackKey : 'callback',
-//            reader      : {
-//                type         : 'json',
-//                rootProperty : 'list.story'
-//            },
-//            extraParams : {
-//                apiKey         : 'MDA4ODE2OTE5MDEzMjYwODI4NDdiOGU5Yw001',
-//                id             : '10001,10003,10002,10004,10005',
-//                requiredAssets : 'image,audio',
-//                fields         : 'parent,title,teaser,storyDate,text,audio,image,artist',
-//                numResults     : 20,
-//                dateType       : 'story',
-//                sort           : 'dateDesc',
-//                action         : 'Or',
-//                output         : 'JSON'
-//            }
-//        }
-//    }
-//});
+
 
 
 
@@ -44,8 +17,7 @@ Ext.define('Music.store.Search', {
 
         proxy : {
             type : 'jsonp',
-//            url  : 'http://discovermusic.moduscreate.com/search.jst'
-            url  : 'http://localhost:9090/search.jst'
+            url  : 'http://discovermusic.moduscreate.com/search.jst'
         }
     }
 
