fe11f068c10865a10681e0d8338048983ee8734e
jaygarcia <jay@moduscreate.com>
2012-08-03 16:07:32 -0400

    updates to the ios project

---
+++ ios/www/silk/actions/getGenres.js
@@ -0,0 +1,10 @@
+global.getGenres_action = function() {
+    var response = Json.encode(fetchGenresExternal(req.data.lastUpdate || new Date().getJulian()));
+    if (req.data.callback) {
+        res.contentType = 'text/javascript';
+
+        response = req.data.callback + '(' + response + ')';
+    }
+    res.write(response);
+    res.stop();
+};
