7af165a8b82ef24e82a2bca6bc0c63ac76ee78a9
jaygarcia <jay@moduscreate.com>
2012-08-03 15:54:29 -0400

    Base iOS branch

---
+++ watch.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+MD5=`md5 /tmp/watcher.md5 | awk '{print $4}'`;
+X=0;
+
+while [ $X -eq 0 ]; do 
+	newMD5=`ls -altR html5/ | grep -v app.json > /tmp/watcher.md5 && md5 /tmp/watcher.md5 | awk '{print $4}'`;
+
+	if [[ "$MD5" != "$newMD5" ]]; then
+		echo 
+		echo "Change found, deploying! `date`"
+		#say "deploying..."
+		./deploy.sh
+		if [ $? -eq 0 ] ; then
+			MD5=$newMD5;
+			#say "done building" >/dev/null 2>&1
+		fi
+	else 
+		printf .	
+	fi
+	
+	sleep .5
+done
