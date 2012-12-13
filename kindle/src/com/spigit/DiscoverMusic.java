592b01b2468d0d08ff62b46ada164e4f0a3ba087
Jay Garcia <jay@moduscreate.com>
2012-12-13 06:45:04 -0500

    added basic kindle stuff

---
+++ kindle/src/com/spigit/DiscoverMusic.java
@@ -0,0 +1,16 @@
+package com.modus;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+
+public class DiscoverMusic extends DroidGap {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
