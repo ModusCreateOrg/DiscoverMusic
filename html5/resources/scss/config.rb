3fac2d3ab846521f0ae8286596e94fba5922460a
Grgur Grisogono <grguru@gmail.com>
2012-12-14 14:53:26 +0100

    Installed Mountain Lion Notification push for compass watch
    http://ghettocooler.net/2012/12/03/mountain-lion-notification-after-sass-compiling/

--- html5/resources/scss/config.rb
+++ html5/resources/scss/config.rb
@@ -9,3 +9,16 @@ sass_path    = dir
 css_path     = File.join(dir, "..", "css")
 environment  = :production
 output_style = :compressed
+
+# Push Mountain Lion Notifications
+EXCEPTIONS = [StandardError, ScriptError]
+begin
+require "notifompass"
+rescue *EXCEPTIONS
+puts "You don't have notifompass gem installed. See http://ghettocooler.net/2012/12/03/mountain-lion-notification-after-sass-compiling/"
+begin
+require "compass-growl"
+rescue *EXCEPTIONS
+puts "You don't have compass-growl installed either!"
+end
+end
