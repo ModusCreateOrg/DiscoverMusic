dba1cf7faec917b2291f0c569dd46cbe2b280ce4
Grgur Grisogono <grguru@gmail.com>
2012-12-17 16:02:38 +0100

    Refactored article view, significantly decreases dom size and interactions

--- html5/app/view/Article.js
+++ html5/app/view/Article.js
@@ -6,51 +6,56 @@
  */
 
 Ext.define('Music.view.Article', {
-    extend: 'Ext.Container',
-    alias : 'widget.article',
+    extend : 'Ext.Container',
+    alias  : 'widget.article',
 
     config : {
         model      : null,
         genre      : null,
-        layout     : 'hbox',
-        scrollable : false,
+        layout     : {
+            type  : 'vbox',
+            align : 'stretch'
+        },
         cls        : 'music-article',
         items      : [
             {
-                xtype      : 'container',
+                xtype      : 'component',
+                cls        : 'music-article-content',
+                itemId     : 'article-content',
                 flex       : 1,
-                cls        : 'music-article-container',
                 scrollable : {
                     direction     : 'vertical',
                     directionLock : true
                 },
-                items : {
-                    xtype : 'component',
-                    cls   : 'music-article-content',
-                    tpl   : new Ext.XTemplate(
-                        '<div class="music-article-image music-article-{genreKey}" style="background-image:url(http://src.sencha.io/600/{image});">',
-                            '<div class="music-article-image-overlay">',
-                                '<h2>{genre}</h2>',
+                tpl        : new Ext.XTemplate(
+                    '<div class="music-article-image music-article-{genreKey}" style="background-image:url(http://src.sencha.io/600/{image});">',
+                        '<div class="music-article-image-overlay">',
+                            '<h2>{genre}</h2>',
 
-                                '<div class="music-article-header music-article-header-portrait">',
-                                    '<div class="music-article-button music-article-button-listen">Listen to Story</div>',
-                                    '<div class="music-article-button music-article-button-favorite">Add to Favorites</div>',
-                                    '<div class="music-article-button music-article-button-tweet">Tweet Story</div>',
-                                '</div>',
+                            '<div class="music-article-header music-article-header-portrait">',
+                                '<div class="music-article-button music-article-button-listen">Listen to Story</div>',
+                                '<div class="music-article-button music-article-button-favorite">Add to Favorites</div>',
+                                '<div class="music-article-button music-article-button-tweet">Tweet Story</div>',
                             '</div>',
                         '</div>',
-                        '<article>',
-                            '<h1>{title}</h1>',
-                            '<h4>{[ this.dateFormat(values.date) ]}</h4>',
-                            '{text}',
-                        '</article>',
-                        {
-                            dateFormat : function(value){
-                                return Ext.util.Format.date(value,'F d, Y');
-                            }
+                    '</div>',
+                    '<article>',
+                        '<h1>{title}</h1>',
+                        '<h4>{[ this.dateFormat(values.date) ]}</h4>',
+                        '{text}',
+                    '</article>',
+                    {
+                        dateFormat : function(value){
+                            return Ext.util.Format.date(value,'F d, Y');
                         }
-                    )
-                }
+                    }
+                )
+            },
+
+            {
+                xtype  : 'component',
+                html   : 'shit',
+                height : 250
             }
         ]
     },
@@ -106,10 +111,9 @@ Ext.define('Music.view.Article', {
 
     applyModel : function(model) {
         var me = this,
-            content = me.down('component[cls=music-article-content]'),
             modelData = model.getData();
 
-        content.setData(modelData);
+        me.down('component#article-content').setData(modelData);
 
         return model;
     }
